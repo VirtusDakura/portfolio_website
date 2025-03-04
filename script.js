20dedb848548a2f8be684ba1ae384d7ae80071e0
VirtusDakura <dakuravirtus@gmail.com>
2025-03-04 14:49:41 +0000

    Added index.html,style.css, and script.js files

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navlinks = document.querySelector('.nav-links');
+
+
+menuIcon.onclick = () => {
+    navlinks.classList.toggle('active');
+}
+
